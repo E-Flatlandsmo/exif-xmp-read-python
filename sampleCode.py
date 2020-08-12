5490e97ddf5238075389e0dad9fe1890e7974891
Eivind Flatlandsmo <Eivind.Flatlandsmo@bkk.no>
2020-08-12 11:50:26 +0200

    Cleaned the code

--- sampleCode.py
+++ sampleCode.py
@@ -1,20 +1,16 @@
-# Add examples for every script, nice and clean
-# Use this as main. FLY when coding!!
-import os
-from pprint import pprint
+image = path_to_image
 
-path_dir = r'C:\Users\ef4342\Pictures\work\DroneAppTest\EivindFlatlandsmo_DroneApp_Test'
-path_images = [os.path.join(path_dir, image) for image in os.listdir(path_dir)]
-image = path_images[0]
-
+# Extract exif 
 from ExifXmpRead.getExif import get_exif_json, get_exif_xml
-# print(get_exif_json(image))
-# print(get_exif_xml(image))
+print(get_exif_json(image))
+print(get_exif_xml(image))
 
+# Extract xmp
 from ExifXmpRead.getXmp import get_xmp_json, get_xmp_xml
-# print(get_xmp_json(image))
-# print(get_xmp_xml(image))
+print(get_xmp_json(image))
+print(get_xmp_xml(image))
 
+# Extract exif and xmp merged to a JSON
 from ExifXmpRead.getExifXmp import get_exif_xmp_json, get_exif_xmp_xml
-# print(get_exif_xmp_json(image))
+print(get_exif_xmp_json(image))
 print(get_exif_xmp_xml(image))
